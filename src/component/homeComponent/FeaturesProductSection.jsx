748d8a43a80c10571dd64297a39af69e7a8cee9b
Ahmedelsayed55 <am1096922@gmail.com>
2025-12-21 19:06:07 +0200

    features section

--- src/component/homeComponent/FeaturesProductSection.jsx
+++ src/component/homeComponent/FeaturesProductSection.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import './featurs.css';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 const FeaturesProductSection = () => {
   return (
-    <div>
-      FeaturesProductSection
+  <div className='w-full'>
+    <div className='container mx-auto mb-5 py-10'>
+    <h2 className='text-2xl font-bold text-center mb-5'>Features Product</h2>
+
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={false}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+          </div>
     </div>
   )
 }
--- src/component/homeComponent/HeroSection.jsx
+++ src/component/homeComponent/HeroSection.jsx
@@ -6,16 +6,14 @@ import img from '../../assets/hero.jpg';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
 import './HeroSwiper.css';
-
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 const HeroSection = () => {
   return (
-  <div className='w-full '>
-    <div className='container mx-auto mb-5  h-[70%]'>
+  <div className='w-full h-[60vh] '>
+    <div className='container mx-auto mb-5 h-full'>
 
  
       <Swiper
@@ -28,19 +26,19 @@ const HeroSection = () => {
         pagination={{
           clickable: true,
         }}
-        navigation={true}
+     
         modules={[Autoplay, Pagination, ]}
-        className="mySwiper"
+        className="mySwiper h-full"
       >
 
         <SwiperSlide>
             <div className="w-full">
-                <img className='object-contain w-full' src={img} alt="" />
+                <img className='object-contain w-full h-full' src={img} alt="" />
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="w-full">
-                <img className='object-contain w-full' src={img} alt="" />
+                <img className='object-contain w-full h-full' src={img} alt="" />
             </div>
         </SwiperSlide>
 
